9dbbf312f86d73e6d9e3963619545bfaf337916e
Lloyd Christopher Malinao <lloxy143@gmail.com>
2021-04-06 01:34:46 +0800

    Initial commit

---
+++ src/js/data/2021-04-05.js
@@ -0,0 +1,491 @@
+dataSetVersion = "2021-04-05"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Filter by Series Entry",
+    key: "series",
+    tooltip: "Check this to restrict to certain series.",
+    checked: false,
+    sub: [
+      { name: "Anime", tooltip: "Initial Characters", key: "s1" },
+      { name: "Game", tooltip: "New characters from the game", key: "g1" },
+    ]
+  },
+  // {
+  //   name: "Filter by Stage Enemy Appearances",
+  //   key: "stage",
+  //   tooltip: "Check this to restrict to characters that appear in certain stages as enemies.",
+  //   checked: false,
+  //   sub: [
+  //     { name: "Stage 1", key: "st1" },
+  //     { name: "Stage 2", key: "st2" },
+  //     { name: "Stage 3", key: "st3" },
+  //     { name: "Stage 4", key: "st4" },
+  //     { name: "Stage 5/Penultimate", tooltip: "Stage 4 in 5-stage games, and Stage 8 in 9-stage games.", key: "st5" },
+  //     { name: "Stage 6/Final", key: "st6" },
+  //     { name: "Stage EX/Phantasm", key: "ex" }
+  //   ]
+  // },
+  // {
+  //   name: "Remove PC-98 Duplicates",
+  //   key: "pc98",
+  //   tooltip: "Check this to remove PC-98 characters with a Windows counterpart."
+  // },
+  // {
+  //   name: "Remove Non-Girls",
+  //   key: "notgirl",
+  //   tooltip: "Check this to remove all non-female characters."
+  // }
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Special Week",
+    img: "EI3yMkt.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Silence Suzuka",
+    img: "tLWCeNw.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Tokai Teio",
+    img: "7v1lJlO.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Vodka",
+    img: "ZGtKHBN.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Air Groove",
+    img: "l4QSzhD.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "El Condor Pasa",
+    img: "CeRKWle.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Oguri Cap",
+    img: "WnStTN0.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Grass Wonder",
+    img: "OHOlyUF.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Gold Ship",
+    img: "9TJtyaa.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Symboli Rudolf",
+    img: "BFevVcR.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Taiki Shuttle",
+    img: "AIxb4bn.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Daiwa Scarlet",
+    img: "9eJVAkh.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "T.M. Opera O",
+    img: "fVhfKvA.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Narita Brian",
+    img: "tgQNl8K.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Hishi Amazon",
+    img: "JHEH5gw.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Fuji Kiseki",
+    img: "Pe41ZNs.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Maruzensky",
+    img: "av3rXFx.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Mejiro McQueen",
+    img: "MPPKKZM.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Seiun Sky",
+    img: "5Sjjrzj.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Yukino Bijin",
+    img: "7arrCdK.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Winning Ticket",
+    img: "kT8JInx.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Tamamo Cross",
+    img: "4RCeIvP.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Seeking the Pearl",
+    img: "wX1vIB6.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Manhattan Cafe",
+    img: "BhZ7R4h.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Tosen Jordan",
+    img: "KKocp3y.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Haru Urara",
+    img: "9QDpQuN.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Kawakami Princess",
+    img: "ZHpE0xH.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Mejiro Ryan",
+    img: "Pi31qLO.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Matikanefukukitaru",
+    img: "MfW5Nq4.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Fine Motion",
+    img: "piPJHvM.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Smart Falcon",
+    img: "kJKWoqr.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Narita Taishin",
+    img: "8KoxSzo.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Air Shakur",
+    img: "Asip4uj.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Gold City",
+    img: "rOOD994.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Super Creek",
+    img: "mtaaOyn.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Inari One",
+    img: "NhPgE0e.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Nishino Flower",
+    img: "ATrkqaM.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Biko Pegasus",
+    img: "CTeJ1rE.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Hishi Akebono",
+    img: "J2HKtC8.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Bamboo Memory",
+    img: "UiB1uRN.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Marvelous Sunday",
+    img: "peID0rR.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Mihono Bourbon",
+    img: "JA1RXYa.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Sweep Tosho",
+    img: "LQHEi2b.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Ines Fujin",
+    img: "amMi3NS.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Biwa Hayahide",
+    img: "IM0PLx6.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Sakura Bakushin O",
+    img: "8b3DDXj.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Shinko Windy",
+    img: "yJvx4ii.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Agnes Tachyon",
+    img: "4GTm1H6.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Zenno Rob Roy",
+    img: "c4vDHUW.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Meisho Doto",
+    img: "dqDieE1.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Rice Shower",
+    img: "9gdsA2F.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Admire Vega",
+    img: "08N10Ic.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Curren Chan",
+    img: "da3Cl21.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Agnes Digital",
+    img: "3s6B805.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Eishin Flash",
+    img: "CEkTMH2.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Nakayama Festa",
+    img: "M6S77so.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Mayano Top Gun",
+    img: "BFSoh60.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Mejiro Dober",
+    img: "OKlSkIS.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Nice Nature",
+    img: "WPQokB7.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "King Halo",
+    img: "wf5iolp.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Matikane Tannhauser",
+    img: "kkBdFji.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Ikuno Dictus",
+    img: "gxyVFIW.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Daitaku Helios",
+    img: "I21XqtI.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+  {
+    name: "Twin Turbo",
+    img: "ZjqabJt.png",
+    opts: {
+      series: ["s1"],
+    }
+  },
+];
